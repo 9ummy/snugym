b9dd84fde8ba7c7da6c550c4ad0f033d490d99cb
EC2 Default User <ec2-user@ip-172-31-35-61.ap-northeast-1.compute.internal>
2021-06-12 18:52:18 +0000

    register complete

--- SNUGYM/app/controllers/users_controller.rb
+++ SNUGYM/app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  def create
+    User.create(name: params[:name],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+    redirect_to :root
+  end
 end
