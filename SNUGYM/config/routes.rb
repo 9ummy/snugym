c2ceca3b0118eee36a1da2502a5fc86a02fc5c22
EC2 Default User <ec2-user@ip-172-31-35-61.ap-northeast-1.compute.internal>
2021-06-12 16:12:58 +0000

    html, css added

--- SNUGYM/config/routes.rb
+++ SNUGYM/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  root 'users#new'
+  
+  resources :users, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
