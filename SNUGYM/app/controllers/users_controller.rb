08504b4549092be451f533fac7c65a5a79bac018
EC2 Default User <ec2-user@ip-172-31-35-61.ap-northeast-1.compute.internal>
2021-06-13 11:35:00 +0000

    working on notification

--- SNUGYM/app/controllers/users_controller.rb
+++ SNUGYM/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to sessions_new_path
     else
-      flash.now[:alert] = "failed to register."
+      flash.now[:alert] = "failed to register"
       render 'new'
     end
   end
