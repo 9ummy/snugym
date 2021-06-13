372cf0e3bdb59b00b5c91d74eebe5c842a593b5e
EC2 Default User <ec2-user@ip-172-31-35-61.ap-northeast-1.compute.internal>
2021-06-13 06:35:23 +0000

    login function made

--- SNUGYM/app/controllers/users_controller.rb
+++ SNUGYM/app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
   
   def create
-    @user = User.new(name: params[:name],
-                     password: params[:password],
-                     password_confirmation: params[:password_confirmation])
-    redirect_to :root
+    user = User.new(name: params[:name],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
+    if user.save
+      redirect_to sessions_new_path
+    else
+      redirect_to '/'
+    end
   end
 end
--- SNUGYM/config/routes.rb
+++ SNUGYM/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'users#new'
   
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
