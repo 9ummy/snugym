054bffe3bc8541220ac6aba807c394fd667af5f4
EC2 Default User <ec2-user@ip-172-31-35-61.ap-northeast-1.compute.internal>
2021-06-13 05:50:26 +0000

    login html, css added

--- SNUGYM/app/controllers/users_controller.rb
+++ SNUGYM/app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def create
-    User.create(name: params[:name],
-                password: params[:password],
-                password_confirmation: params[:password_confirmation])
+    @user = User.new(name: params[:name],
+                     password: params[:password],
+                     password_confirmation: params[:password_confirmation])
     redirect_to :root
   end
 end
